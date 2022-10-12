2469f568a1a2fc8584a607fa3869942b9dfaa410
Hex <lihex.go@gmail.com>
2022-10-12 13:31:15 +0800

    refactor: refactor DummyLog with xpretty

--- dummy.go
+++ dummy.go
@@ -10,44 +10,21 @@ import (
 
 	"github.com/coghost/xpretty"
 	"github.com/gookit/goutil/dump"
-	"github.com/thoas/go-funk"
 )
 
 var (
 	Green = xpretty.Green
 	Red   = xpretty.Red
-	faint = xpretty.Faint
-	info  = xpretty.Info
 )
 
 // DummyLog will print a dummy log with green bg
 func DummyLog(msg ...interface{}) {
-	if !uc.dummyLog {
-		return
-	}
-	c, l := Caller(2)
-
-	if funk.IsEmpty(msg) {
-		fmt.Printf("%v %v(%v) %v\n", faint(StrNow(DFmt2)), Green(c), Green(l), info("dummy log"))
-	} else {
-		s := AnyJoin(", ", msg...)
-		fmt.Printf("%v %v(%v) %v\n", faint(StrNow(DFmt2)), Green(c), Green(l), info(s))
-	}
+	xpretty.DLog(3, Green, msg...)
 }
 
 // DummyErrorLog will print a dummy log with red bg
 func DummyErrorLog(msg ...interface{}) {
-	if !uc.dummyLog {
-		return
-	}
-	c, l := Caller(2)
-
-	if funk.IsEmpty(msg) {
-		fmt.Printf("%v %v(%v) %v\n", faint(StrNow(DFmt2)), Red(c), Red(l), info("dummy log"))
-	} else {
-		s := AnyJoin(", ", msg...)
-		fmt.Printf("%v %v(%v) %v\n", faint(StrNow(DFmt2)), Red(c), Red(l), info(s))
-	}
+	xpretty.DLog(3, xpretty.Red, msg...)
 }
 
 // DumpCallerStack
@@ -106,12 +83,13 @@ func PauseToDebug(msg ...string) bool {
 // Pause: press enter to continue
 func Pause(args ...string) {
 	msg := FirstOrDefaultArgs("Press Enter to continue", args...)
+	exit := xpretty.Faint("(Ctrl+C to exit)")
 
 	pc, _, l, _ := runtime.Caller(1)
 	c := runtime.FuncForPC(pc).Name()
 	fmt.Printf("===> %v(%v) <===\n", c, l)
 
-	xpretty.YellowPrintf("[%v] %v:", StrNow(), msg)
+	xpretty.YellowPrintf("[%s] %s%s:", StrNow(), msg, exit)
 	reader := bufio.NewReader(os.Stdin)
 	_, e := reader.ReadString('\n')
 	if e != nil {
